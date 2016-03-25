ea22766883addfe89accd3daa4be829e1b3b0c69
SC <github.com@genkisugi.net>
2016-03-24 18:22:40 -0700

    ensure that we are reasonably simulating run_promise_functions via the 'base' engine; work on geneontology/amigo#323

--- tests/engines.test.js
+++ tests/engines.test.js
@@ -196,3 +196,79 @@ describe('bbop-manager-golr: jquery engine test', function(){
     });
 
 });
+
+
+describe('bbop-manager-golr: jquery engine test for coordination', function(){
+
+    var mock_jQuery = null;
+    var engine_to_use = null;
+    var gconf = null;
+    before(function(done){
+        // Modify the manager into functioning--will need this to get
+        // tests working for jQuery in this environment.
+        var domino = require('domino');
+        mock_jQuery = require('jquery')(domino.createWindow());
+        var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+        mock_jQuery.support.cors = true;
+        mock_jQuery.ajaxSettings.xhr = function() {
+	    return new XMLHttpRequest();
+        };
+	
+	// Setup a reusable engine and manager.
+	// We need to do some fiddling to get the jQuery manager to
+	// operate correctly in this land of node.
+	engine_to_use = new jquery_engine(golr_response);
+	engine_to_use.JQ = mock_jQuery;
+	
+	gconf = new golr_conf.conf(amigo.data.golr);
+
+	done();
+    });
+
+    it('jquery registered callback', function(done){
+	
+    	engine_to_use.method('GET');
+    	var m = new golr_manager(golr_url, gconf, engine_to_use, 'async');
+	
+	//
+	assert.isFunction(m.run_promise_functions, 'should have inherited this');
+
+	//
+		// Define three promise producing functions.
+	function f1(){
+    	    return m.search();
+	}
+	function f2(){
+    	    return m.search();
+	}
+	function f3(){
+    	    return m.search();
+	}
+
+	var count = 0;
+	function acc_fun(resp, man){
+	    count++;
+	}
+
+	function fin_fun(man){
+    	    assert.equal(count, 3, 'got three calls: ' + count);
+    	    // assert.equal('bbop-manager-golr.jquery', man._is_a,
+	    // 		 'right manager: ' + man._is_a);
+    	    done();
+	}
+
+	function err_fun(err, man){
+	    console.log(err);
+	    console.log(man);
+	    console.log(man._is_a);
+    	    assert.isTrue(false,
+			  'should not have hit an error here: '+err.toString());
+    	    done();
+	}
+
+	// Run.
+	m.run_promise_functions([f1, f2, f3], acc_fun, fin_fun, err_fun);
+
+    });
+});
+
